475c8260244d75c25bd02eae5fd21c5e8b6f87d4
Wei Liu <wei.liu2718@outlook.com>
2025-06-26 17:53:22 +0800

    cmd test

--- test_cmd.cpp
+++ test_cmd.cpp
@@ -1,13 +1,15 @@
 #include <CLI11.hpp>
 
 int main(int argc, char **argv) {
-  CLI::App app{
-      "This is a test program for c++ command line parser library."};
+  CLI::App app{"This is a test program for c++ command line parser library."};
   std::string file;
-  app.add_option("-f,--file", file, "Require an existing file")
-      ->required()
-      ->check(CLI::ExistingFile);
+  app.add_option("-f,--file", file, "Require an existing file")->required();
 
-  app.parse(argc, argv);
+  try {
+    app.parse(argc, argv);
+  } catch (const CLI::ParseError &e) {
+    std::cout << e.get_name() << std::endl;
+    app.exit(e);
+  }
   return 0;
 }
